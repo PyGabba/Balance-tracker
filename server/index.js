f530869f6b20407f3e7acbffdbff0ea6377927c0
PyGabba <124427614+PyGabba@users.noreply.github.com>
2026-03-30 09:27:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const axios = require('axios');
+
+const url = `https://poke-whp.onrender.com/`; // Render URL
+const interval = 300000; // Interval in milliseconds (300 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
