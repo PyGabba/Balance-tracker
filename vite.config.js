88095eaefcfbfde300642c6ff29a9c9e1837ef24
PyGabba <124427614+PyGabba@users.noreply.github.com>
2026-03-31 23:17:20 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      external: ['tesseract.js'],
       onwarn(warning, warn) {
         if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return;
         warn(warning);
