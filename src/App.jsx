3204e14dfc0911d9f97e2187b27397bc51190730
PyGabba <124427614+PyGabba@users.noreply.github.com>
2026-04-05 07:24:49 +0200

    Modify footer styles in App component

--- src/App.jsx
+++ src/App.jsx
@@ -319,11 +319,11 @@ function TabBar({ tab, setTab, householdId }) {
       justifyContent: "space-around",
       background: "#0E0E16",            // darker background
       borderTop: "1px solid #2A2A3E",
-      // padding: "12px 0 max(12px, env(safe-area-inset-bottom, constant(safe-area-inset-bottom)))",
-      padding: "20px",
-      position: "sticky",
+      padding:  "8px 0 max(12px, env(safe-area-inset-bottom)) ",
+      // position: "sticky",
       bottom: 0,
       boxShadow: "0 -2px 12px rgba(0,0,0,0.4)",  // subtle top shadow
+      flexShrink: 0,
     }}>
       {tabs.map(t => {
         const isActive = tab === t.id;
